25af3b6097fe16cf3f76167560c10c12a9db55e2
Bill Ramsey <bill@vybly.com>
2011-08-23 18:14:46 -0400

    Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skills::Application.config.secret_token = '884a423a717dfdbb228319c44d4fef11ad1114035af31d9eee13d2a9b68355afa28276cf3a0927ddd1b44106121858b9bc0fd2bc4a93cc418c4ffab142d84e13'
