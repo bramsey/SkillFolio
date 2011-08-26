e1eb0c03348c154282a962d7c3a79a5a78ea0ade
Bill Ramsey <bill@vybly.com>
2011-08-26 16:32:35 -0400

    added js to limit field addition based on current number of levels

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,3 +10,35 @@ $('#container').delegate('.met_check', 'click', function () {
 		}
 	});
 });
+
+
+$('form a.add_nested_fields').live('click', function () {
+	if ($(this).attr('data-association') === 'levels'){
+		siblings = $(this).siblings('.fields:visible');
+		if(siblings.length >= 3){
+			$(this).hide();
+		}
+	}
+});
+$('form a.remove_nested_fields').live('click', function () {
+	siblings = $(this).parentsUntil('.skill').children('.fields:visible');
+	if(siblings.length <= 4){
+		$(this).closest('.skill').children('.add_nested_fields').show();
+	}
+});
+
+$(document).ready(function() {
+    var fields = $('.skill').children('.add_nested_fields');
+
+	if (fields.length > 0) {
+		
+		fields.each(function(){
+			siblings = $(this).siblings('.fields');
+			if(siblings.length >= 3){
+				$(this).hide();
+			} else {
+				$(this).show();
+			}
+		});
+	}
+});
