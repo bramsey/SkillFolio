30e323fbd68e414ed24a9c523887c37150148626
Bill Ramsey <bill@vybly.com>
2011-09-24 15:54:42 -0400

    added navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,21 @@ module ApplicationHelper
   def level_rank( level )
     ((level.skill.levels.index(level) + 1).to_f/level.skill.levels.length.to_f*10).to_i.to_s
   end
+  
+  def siteNav
+    if user_signed_in?
+      link("Dashboard", root_path) + " " +
+      link("Domains", domains_path)
+    else
+      #put logged_out links here.
+    end
+  end
+  
+  def link(label, path)
+    if request.fullpath == path || label.downcase == params[:controller]
+      link_to label, path, :class => 'selected'
+    else
+      link_to label, path
+    end
+  end
 end
